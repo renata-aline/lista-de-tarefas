ff66bf5210c140e9e1abc941f5ed7132bb495992
renata-aline <renatalinec@gmail.com>
2023-12-27 10:09:28 -0300

    feito filter de tarefas completadas  e incompletas

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,11 @@ function App() {
     },
   ]);
 
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newtodos = [
@@ -63,20 +67,27 @@ function App() {
     <div className="app">
       <h1>Lista de tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter />
+      <Filter filter={filter} setFilter={setFilter} />
       <div className="todo-list">
         {todos
-        .filter((todo) => todo.text.toLowerCase()
-        .includes(search.toLowerCase())
-        )
-        .map((todo) => (
-          <Todo
-            key={todo.id}
-            todo={todo}
-            removeTodo={removeTodo}
-            completeTodo={completeTodo}
-          />
-        ))}
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
       </div>
       <TodoForm addTodo={addTodo} />
     </div>
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,14 +1,14 @@
-const Filter = () => {
+const Filter = ({ filter, setFilter }) => {
   return (
     <div className="filter">
       <h2>Filtrar:</h2>
       <div className="filter-options">
         <div>
           <p>Status:</p>
-          <select>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
             <option value="All">Todas</option>
             <option value="Completed">Completas</option>
-            <option value="Incompleted">Incompletas</option>
+            <option value="Incomplete">Incompletas</option>
           </select>
         </div>
         <div>
