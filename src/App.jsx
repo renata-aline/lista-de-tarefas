d5f16410900855dfb5982553fa76b6568f6d73fe
renata-aline <renatalinec@gmail.com>
2023-12-27 10:44:33 -0300

    feito filter por ordem alfabetica

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ function App() {
     <div className="app">
       <h1>Lista de tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter filter={filter} setFilter={setFilter} />
+      <Filter filter={filter} setFilter={setFilter}  setSort={setSort}/>
       <div className="todo-list">
         {todos
           .filter((todo) =>
@@ -80,6 +80,12 @@ function App() {
           .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
           )
+          .sort((a,b) =>
+          sort === "Asc"
+          ? a.text.localeCompare(b.text)
+          : b.text.localeCompare(a.text)
+          )
+
           .map((todo) => (
             <Todo
               key={todo.id}
